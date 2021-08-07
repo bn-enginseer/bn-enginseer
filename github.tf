86318d715d7d6debe32388f6ddb499f77a48757f
BN Developer <dev@bndigital.co>
2021-08-07 04:51:14 +0300

    Added protection rule
    Signed-off-by: BN Developer <dev@bndigital.co>

--- github.tf
+++ github.tf
@@ -1,17 +1,44 @@
 provider "github" {
-  alias = "employee"
   owner = "bn-enginseer"
 }
 
 provider "github" {
   alias = "organization"
   owner = "bn-digital"
+  token = var.github_org_token
+}
+
+resource "github_repository" "bn-enginseer" {
+  name =                   "bn-enginseer"
+  description =            "@bn-digital Software Engineer Avatar"
+  homepage_url =           "https://bndigital.github.io"
+  visibility =             "public"
+  license_template =       "Unlicense"
+  topics =                 ["terraform"]
+  has_projects =           false
+  has_issues =             false
+  auto_init =              false
+  delete_branch_on_merge = true
+}
+
+resource "github_branch_default" "latest" {
+  branch =     "latest"
+  repository = github_repository.bn-enginseer.id
+}
+
+// noinspection MissingProperty
+resource "github_branch_protection" "admin-only-approval" {
+  pattern =       github_repository.bn-enginseer.default_branch
+  repository_id = github_repository.bn-enginseer.id
+  required_pull_request_reviews {
+    require_code_owner_reviews =      true
+    required_approving_review_count = 1
+  }
 }
 
 data "github_repository_pull_requests" "employee" {
-  provider =        github.employee
-  base_repository = "bn-enginseer"
-  base_ref =        "latest"
+  base_repository = github_repository.bn-enginseer.name
+  base_ref =        github_repository.bn-enginseer.default_branch
   sort_by =         "created"
   sort_direction =  "asc"
   state =           "closed"
